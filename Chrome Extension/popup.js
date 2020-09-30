acaef7deefa0955a662c58cd44cdd3b910059c1a
Pratik Pathak <pratikpathak200@gmail.com>
2020-09-30 13:16:24 +0530

    error resolved

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,41 +1,44 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var sub = document.getElementById('sub');
-    sub.addEventListener('click', function() {
+    if(sub){
+        sub.addEventListener('click', function() {
 
-        var news = document.getElementById('news').value;
+            var news = document.getElementById('news').value;
 
-        var request = new XMLHttpRequest();
-        var url = 'https://fakenews1.azurewebsites.net/api/News?name=' + news
-        request.open('GET', url, true)
-        request.onload = function() {
-            alert(this.response)
-            document.getElementById("finalAns").innerHTML = "<h2>"+this.response+"</h2>"
+            var request = new XMLHttpRequest();
+            var url = 'https://fakenews1.azurewebsites.net/api/News?name=' + news
+            request.open('GET', url, true)
+            request.onload = function() {
+                alert(this.response)
+                document.getElementById("finalAns").innerHTML = "<h2>"+this.response+"</h2>"
 
-        }
-
-        request.send()
-
-        var i = 0;
-         
-        if (i == 0) {
-            i = 1;
-            var elem = document.getElementById("myBar");
-            var width = 10;
-            var id = setInterval(frame, 150);
-            function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innerHTML = width  + "%";
             }
+
+            request.send()
+
+            var i = 0;
+            
+            if (i == 0) {
+                i = 1;
+                var elem = document.getElementById("myBar");
+                var width = 10;
+                var id = setInterval(frame, 150);
+                function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    elem.style.width = width + "%";
+                    elem.innerHTML = width  + "%";
+                }
+                }
             }
-        }
-        
+            
 
-
+          
+    
     });
+}
 });
